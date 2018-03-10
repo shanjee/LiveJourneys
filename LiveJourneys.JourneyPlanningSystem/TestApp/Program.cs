7fa706d52ec9bcff73ac26f95daa26b890ab8a12
Shanjeeva Kugathasan <Shanjeeva@CreativeSoftware.com>
2018-03-10 12:39:45 +0530

    test console added

---
+++ LiveJourneys.JourneyPlanningSystem/TestApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using LiveJourneys.JourneyPlanningSystem.Data;
+using LiveJourneys.JourneyPlanningSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JourneyPlanningSystemDbContext context = new JourneyPlanningSystemDbContext();
+            BasicEFRepository<StationMapping> basicEFRepository = new BasicEFRepository<StationMapping>(context);
+            var dataList =  basicEFRepository.GetAll();
+
+            foreach (var item in dataList)
+            {
+                Console.WriteLine($"{item.FromStaionId} - {item.Distance.ToString()}");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
